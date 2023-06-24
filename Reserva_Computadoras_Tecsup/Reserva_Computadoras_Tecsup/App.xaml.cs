7aadbcf176730689f750ba8a52cd6fda59450797
dannycastilloo <dannycastillootiniano@gmail.com>
2023-06-24 10:37:54 -0500

    Agregar archivos de proyecto.

---
+++ Reserva_Computadoras_Tecsup/Reserva_Computadoras_Tecsup/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration;
+using Reserva_Computadoras_Tecsup.Views;
+
+namespace Reserva_Computadoras_Tecsup
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Reserva_Computadoras_Tecsup/Reserva_Computadoras_Tecsup/Views/Reservar1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Reserva_Computadoras_Tecsup.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Reservar1 : ContentPage
+    {
+        public Reservar1()
+        {
+            InitializeComponent();
+            List<string> computadoras = new List<string>
+            {
+                "PC-01",
+                "PC-02",
+                "PC-03",
+                "PC-04",
+                "PC-05",
+                "PC-06",
+                "PC-07",
+                "PC-08",
+                "PC-09",
+                "PC-10",
+                "PC-01",
+                "PC-02",
+                "PC-03",
+                "PC-04",
+                "PC-05",
+                "PC-06",
+                "PC-07",
+                "PC-08",
+                "PC-09",
+                "PC-10"
+            };
+            misComputadoras.ItemsSource = computadoras;
+        }
+    }
+}
