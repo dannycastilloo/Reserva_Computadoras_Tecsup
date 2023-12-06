891fd87a7a62684480a9d413147cd9425da997cb
Diego <diego.valdez.r@tecsup.edu.pe>
2023-12-06 12:45:23 -0500

    Modelo y datos

--- Reserva_Computadoras_Tecsup/Reserva_Computadoras_Tecsup/Models/Reservation.cs
+++ Reserva_Computadoras_Tecsup/Reserva_Computadoras_Tecsup/Models/Reservation.cs
@@ -6,10 +6,14 @@ namespace Reserva_Computadoras_Tecsup.Models
 {
     public class Reservation
     {
-        public int Id { get; set; }
-        public int IdUsuario { get; set; }
-        public int IdComputer { get; set; }
+        public int ReserveId { get; set; }
+        public int UserId { get; set; }
+        public int ComputerId { get; set; }
         public DateTime FechaHoraInicio { get; set; }
         public DateTime FechaHoraFin { get; set; }
+        public string State { get; set; }
+        public string key { get; set; }
+        public bool Active { get; set; }
+
     }
 }
--- Reserva_Computadoras_Tecsup/Reserva_Computadoras_Tecsup/Models/Usuario.cs
+++ Reserva_Computadoras_Tecsup/Reserva_Computadoras_Tecsup/Models/Usuario.cs
@@ -6,12 +6,13 @@ namespace Reserva_Computadoras_Tecsup.Models
 {
     public class Usuario
     {
-        public int Id { get; set; }
-        public string Nombres { get; set; }
-        public string Apellidos { get; set; }
-        public string Carrera { get; set; }
-        public string Correo { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Career{ get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
-        public string Rol { get; set; }
+        public string Phone { get; set; }
+        public string Role { get; set; }
     }
 }
